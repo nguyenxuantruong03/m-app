57344717b6386f988de48b499fca2ca872d50c0c
asdiop159752 <nxt159753@gmail.com>
2024-04-02 12:55:27 +0700

    Fix: url navbar Auth (URL)

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -11,6 +11,7 @@ import { Home, Server, Settings, User } from "lucide-react";
 export const Navbar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const url = process.env.NEXT_PUBLIC_URL;
   return (
     <nav className="bg-secondary flex justify-between items-center p-4 rouned-xl w-[600px] shadow-sm">
       <div className="flex gap-x-1">
@@ -28,7 +29,7 @@ export const Navbar = () => {
           asChild
           variant={pathname === "/client" ? "default" : "outline"}
         >
-          <Link href="/client" className="flex items-center justify-center">
+          <Link href={`${url}/client`} className="flex items-center justify-center">
             <User className="w-4 h-4 mr-1" />
             Client
           </Link>
@@ -37,7 +38,7 @@ export const Navbar = () => {
           asChild
           variant={pathname === "/server" ? "default" : "outline"}
         >
-          <Link href="/server" className="flex items-center justify-center">
+          <Link href={`${url}/server`} className="flex items-center justify-center">
             <Server className="w-4 h-4 mr-1" />
             Server
           </Link>
@@ -46,7 +47,7 @@ export const Navbar = () => {
           asChild
           variant={pathname === "/infouser" ? "default" : "outline"}
         >
-          <Link href="/infouser" className="flex items-center justify-center">
+          <Link href={`${url}/infouser`} className="flex items-center justify-center">
             <Settings className="w-4 h-4 mr-1" />
             Setting
           </Link>
