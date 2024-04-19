eddb343b6fef032ef00b704106b0a65e7894be66
asdiop159752 <nxt159753@gmail.com>
2024-04-19 11:29:47 +0700

    Fix: update missing role file .d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,7 +1,7 @@
 import NextAuth,{type DefaultSession} from "next-auth"
 
 export type ExtendedUser = DefaultSession["user"] & {
-    role: "ADMIN" | "USER" | "STAFF";
+    role: "ADMIN" | "USER" | "STAFF"  | "SHIPPER" | "GUEST" | "MARKETING";
     isTwoFactorEnabled: boolean;
     isOAuth: boolean;
     imageCredential: string[];
